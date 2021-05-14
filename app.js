bd63c865b751c3d53efee6909847b14388d85785
loveefraimsson <loveefraimsson@gmail.com>
2021-05-14 14:44:10 +0200

    Ändring av default startsida

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {index:false}));
 
-app.use('/', indexRouter);
+app.use('/', booksRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 
